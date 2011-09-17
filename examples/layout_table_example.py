1b8002f7d3d046b7cce21a70c35b192f7094a17e
Sebastian Pölsterl <sebp@k-d-w.org>
2011-09-17 19:07:20 +0200

    Make sure table example looks the same as grid example

--- examples/layout_table_example.py
+++ examples/layout_table_example.py
@@ -3,7 +3,7 @@ from gi.repository import Gtk
 class TableWindow(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title="Table Demo")
+        Gtk.Window.__init__(self, title="Table Example")
 
         table = Gtk.Table(3, 3, True)
         self.add(table)
@@ -13,12 +13,14 @@ class TableWindow(Gtk.Window):
         button3 = Gtk.Button(label="Button 3")
         button4 = Gtk.Button(label="Button 4")
         button5 = Gtk.Button(label="Button 5")
+        button6 = Gtk.Button(label="Button 6")
 
         table.attach(button1, 0, 1, 0, 1)
         table.attach(button2, 1, 3, 0, 1)
         table.attach(button3, 0, 1, 1, 3)
         table.attach(button4, 1, 3, 1, 2)
         table.attach(button5, 1, 2, 2, 3)
+        table.attach(button6, 2, 3, 2, 3)
 
 win = TableWindow()
 win.connect("delete-event", Gtk.main_quit)
