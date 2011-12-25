1233898a04556c5fcc37ec91fc45c5cb26955cf3
Sebastian Pölsterl <sebp@k-d-w.org>
2011-12-25 12:02:51 +0100

    Added section describing Gtk.Builder

---
+++ examples/builder_example.py
@@ -0,0 +1,17 @@
+from gi.repository import Gtk
+
+class Handler:
+    def onDeleteWindow(self, *args):
+        Gtk.main_quit(*args)
+
+    def onButtonPressed(self, button):
+        print "Hello World!"
+
+builder = Gtk.Builder()
+builder.add_from_file("builder_example.glade")
+builder.connect_signals(Handler())
+
+window = builder.get_object("window1")
+window.show_all()
+
+Gtk.main()
