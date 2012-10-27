1a3fde68936c2cf3ad8dfe78ab052749ec3b6965
Sebastian Pölsterl <sebp@k-d-w.org>
2012-10-27 19:14:58 +0200

    Raise version to 3.4 which reflects the version of pygobject

--- source/conf.py
+++ source/conf.py
@@ -50,7 +50,7 @@ locale_dirs = ["../translations/locale",]
 # built documents.
 #
 # The short X.Y version.
-version = '3.0'
+version = '3.4'
 # The full version, including alpha/beta/rc tags.
 release = '1.0'
 
