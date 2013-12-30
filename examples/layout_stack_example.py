73f5d15e9ed52ba74ad461b0425e92c6fffd578e
Gian Mario Tagliaretti <gianmt@gnome.org>
2013-12-30 10:15:10 +0100

    Add Gtk.Stack and Gtk.StackSwitcher example

---
+++ examples/layout_stack_example.py
@@ -0,0 +1,31 @@
+from gi.repository import Gtk
+
+class StackWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Stack Demo")
+        self.set_border_width(10)
+
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
+
+        stack = Gtk.Stack()
+        stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
+        stack.set_transition_duration(1000)
+        
+        checkbutton = Gtk.CheckButton("Click me!")
+        stack.add_titled(checkbutton, "check", "Check Button")
+        
+        label = Gtk.Label()
+        label.set_markup("<big>A fancy label</big>")
+        stack.add_titled(label, "label", "A label")
+
+        stack_switcher = Gtk.StackSwitcher()
+        stack_switcher.set_stack(stack)
+        vbox.pack_start(stack_switcher, True, True, 0)
+        vbox.pack_start(stack, True, True, 0)
+
+win = StackWindow()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
