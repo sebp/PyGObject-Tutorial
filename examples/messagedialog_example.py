449872b71d589c1225fdfbb905b5bbc7d7edd2d5
Gian Mario Tagliaretti <g.tagliaretti@gmail.com>
2020-07-08 23:56:07 +0200

    Update messagedialog_example.py
    Remove deprecation warnings from the message dialog example
    
    Fixes #174

--- examples/messagedialog_example.py
+++ examples/messagedialog_example.py
@@ -11,30 +11,28 @@ class MessageDialogWindow(Gtk.Window):
         box = Gtk.Box(spacing=6)
         self.add(box)
 
-        button1 = Gtk.Button("Information")
+        button1 = Gtk.Button(label="Information")
         button1.connect("clicked", self.on_info_clicked)
         box.add(button1)
 
-        button2 = Gtk.Button("Error")
+        button2 = Gtk.Button(label="Error")
         button2.connect("clicked", self.on_error_clicked)
         box.add(button2)
 
-        button3 = Gtk.Button("Warning")
+        button3 = Gtk.Button(label="Warning")
         button3.connect("clicked", self.on_warn_clicked)
         box.add(button3)
 
-        button4 = Gtk.Button("Question")
+        button4 = Gtk.Button(label="Question")
         button4.connect("clicked", self.on_question_clicked)
         box.add(button4)
 
     def on_info_clicked(self, widget):
-        dialog = Gtk.MessageDialog(
-            self,
-            0,
-            Gtk.MessageType.INFO,
-            Gtk.ButtonsType.OK,
-            "This is an INFO MessageDialog",
-        )
+        dialog = Gtk.MessageDialog(parent=self,
+                                   flags=0,
+                                   message_type=Gtk.MessageType.INFO,
+                                   buttons=Gtk.ButtonsType.OK,
+                                   text="This is an INFO MessageDialog")
         dialog.format_secondary_text(
             "And this is the secondary text that explains things."
         )
@@ -44,13 +42,11 @@ class MessageDialogWindow(Gtk.Window):
         dialog.destroy()
 
     def on_error_clicked(self, widget):
-        dialog = Gtk.MessageDialog(
-            self,
-            0,
-            Gtk.MessageType.ERROR,
-            Gtk.ButtonsType.CANCEL,
-            "This is an ERROR MessageDialog",
-        )
+        dialog = Gtk.MessageDialog(parent=self,
+                                   flags=0,
+                                   message_type=Gtk.MessageType.ERROR,
+                                   buttons=Gtk.ButtonsType.CANCEL,
+                                   text="This is an ERROR MessageDialog")
         dialog.format_secondary_text(
             "And this is the secondary text that explains things."
         )
@@ -60,13 +56,11 @@ class MessageDialogWindow(Gtk.Window):
         dialog.destroy()
 
     def on_warn_clicked(self, widget):
-        dialog = Gtk.MessageDialog(
-            self,
-            0,
-            Gtk.MessageType.WARNING,
-            Gtk.ButtonsType.OK_CANCEL,
-            "This is an WARNING MessageDialog",
-        )
+        dialog = Gtk.MessageDialog(parent=self,
+                                   flags=0,
+                                   message_type=Gtk.MessageType.WARNING,
+                                   buttons=Gtk.ButtonsType.OK_CANCEL,
+                                   text="This is an WARNING MessageDialog")
         dialog.format_secondary_text(
             "And this is the secondary text that explains things."
         )
@@ -79,13 +73,11 @@ class MessageDialogWindow(Gtk.Window):
         dialog.destroy()
 
     def on_question_clicked(self, widget):
-        dialog = Gtk.MessageDialog(
-            self,
-            0,
-            Gtk.MessageType.QUESTION,
-            Gtk.ButtonsType.YES_NO,
-            "This is an QUESTION MessageDialog",
-        )
+        dialog = Gtk.MessageDialog(parent = self,
+                                   flags = 0,
+                                   message_type = Gtk.MessageType.QUESTION,
+                                   buttons = Gtk.ButtonsType.YES_NO,
+                                   text = "This is an QUESTION MessageDialog")
         dialog.format_secondary_text(
             "And this is the secondary text that explains things."
         )
