ba1c888fe9d70fc017551727392421dc2bb3c518
Chris Mayo <aklhfex@gmail.com>
2020-12-18 19:17:33 +0000

    Improve the Popovers chapter
    - Use more of the features of Gtk.MenuButton
    - Ensure the menu can be seen in the advanced example
    - Consistently create objects using GObject constructors with
      named arguments
    - Rework some of the text

--- examples/popover_example.py
+++ examples/popover_example.py
@@ -13,21 +13,16 @@ class PopoverWindow(Gtk.Window):
         outerbox = Gtk.Box(spacing=6, orientation=Gtk.Orientation.VERTICAL)
         self.add(outerbox)
 
-        button = Gtk.Button.new_with_label("Click Me")
-        button.connect("clicked", self.on_click_me_clicked)
-        outerbox.pack_start(button, False, True, 0)
-
         self.popover = Gtk.Popover()
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         vbox.pack_start(Gtk.ModelButton(label="Item 1"), False, True, 10)
         vbox.pack_start(Gtk.Label(label="Item 2"), False, True, 10)
+        vbox.show_all()
         self.popover.add(vbox)
         self.popover.set_position(Gtk.PositionType.BOTTOM)
 
-    def on_click_me_clicked(self, button):
-        self.popover.set_relative_to(button)
-        self.popover.show_all()
-        self.popover.popup()
+        button = Gtk.MenuButton(label="Click Me", popover=self.popover)
+        outerbox.pack_start(button, False, True, 0)
 
 
 win = PopoverWindow()
