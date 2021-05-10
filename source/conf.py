94935795d91dad697012de8824d4c81c24bab1cd
Chris Mayo <aklhfex@gmail.com>
2021-05-10 19:32:17 +0100

    Expand the description of Gtk.TreeViewColumn
    Mention column headers being optional, some pointers on custom renderers
    and more on the mapping from model to view column.

--- source/conf.py
+++ source/conf.py
@@ -38,6 +38,7 @@ intersphinx_mapping = {
     "gobject": ("https://lazka.github.io/pgi-docs/GObject-2.0", None),
     "gio": ("https://lazka.github.io/pgi-docs/Gio-2.0", None),
     "gtk": ("https://lazka.github.io/pgi-docs/Gtk-3.0", None),
+    "pygobject": ("https://pygobject.readthedocs.io/en/latest", None),
 }
 
 # Add any paths that contain templates here, relative to this directory.
